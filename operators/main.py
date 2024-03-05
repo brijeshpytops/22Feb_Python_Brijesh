7731713589ce6d393b7b4aca01e483c9ce1c3f47
brijeshpytops <brijeshgondaliya.tops@gmail.com>
2024-03-05 08:39:55 +0530

    operators

---
+++ operators/main.py
@@ -0,0 +1,167 @@
+"""
+
+Arithmetic Operators:
+
+Addition (+)
+Subtraction (-)
+Multiplication (*)
+Division (/)
+Floor Division (//)
+Modulus (%)
+Exponentiation (**)
+
+
+Comparison Operators:
+
+Equal to (==)
+Not equal to (!=)
+Greater than (>)
+Less than (<)
+Greater than or equal to (>=)
+Less than or equal to (<=)
+
+Logical Operators:
+
+Logical AND (and)
+Logical OR (or)
+Logical NOT (not)
+
+Assignment Operators:
+
+Assignment (=)
+Add and assign (+=)
+Subtract and assign (-=)
+Multiply and assign (*=)
+Divide and assign (/=)
+Modulus and assign (%=)
+Floor divide and assign (//=)
+Exponentiate and assign (**=)
+Bitwise AND and assign (&=)
+Bitwise OR and assign (|=)
+Bitwise XOR and assign (^=)
+Bitwise left shift and assign (<<=)
+Bitwise right shift and assign (>>=)
+
+Bitwise Operators:
+
+Bitwise AND (&)
+Bitwise OR (|)
+Bitwise XOR (^)
+Bitwise NOT (~)
+Bitwise left shift (<<)
+Bitwise right shift (>>)
+
+Membership Operators:
+
+in: Evaluates to True if a sequence contains a specified value.
+not in: Evaluates to True if a sequence does not contain a specified value.
+Identity Operators:
+
+is: Evaluates to True if the variables on either side of the operator point to the same object.
+is not: Evaluates to True if the variables on either side of the operator do not point to the same object.
+"""
+
+# num1 = 2
+# num2 = 3
+
+# print(num1 + num2)
+# print(num1 - num2)
+# print(num1 * num2)
+# print(num1 / num2)
+# print(num1 // num2)
+# print(num1 ** num2)
+
+
+# num1 = 10
+# num1 = num1 + 20
+# num1 += 20
+# num1 *= 20
+# print(num1)
+
+# num1 = 10
+# num2 = 10
+# num3 = 20
+
+# print(num1 == num2)
+# print(num1 != num2)
+# print(num1 < num2)
+# print(num1 > num2)
+# print(num1 <= num2)
+# print(num1 >= num2)
+
+# A B C and or
+# T T T T   T
+# F T T F   T
+# T F T F   T
+# T T F F   T
+# F F F F   F
+
+# A = 1
+# B = 0
+# C = True
+# D = False
+# E = 10 < 20
+# F = 10 != 20
+
+# print(not C)
+# print(not D)
+
+# print(A and B)
+# print(C and D)
+# print(E and F and A and B )
+
+# print(A or B)
+
+# print(E or F or A or B )
+
+# nums = [1,2,3,4,5]
+# name = "In Python, operators are special symbols or keywords that are used to perform operations on operands. Operands are the values or variables on which the operations are performed. Python supports various types of operators, including arithmetic operators, comparison operators, logical operators, assignment operators, bitwise operators, Membership operators, and identity operators. Here's a brief overview of each type:".lower()
+
+# print(3 in nums)
+# print(3 not in nums)
+# print(10 not in nums)
+# print("MemBerShip".lower() in name)
+
+# num1 = 10
+# num2 = 20
+# num3 = 10
+
+# print(num1 is num2)
+# print(num1 is num3)
+# print(id(num1))
+# print(id(num3))
+
+# nums = [1,2]
+# nums1 = nums
+# nums = [1,2]
+# nums1 = nums.copy()
+# print(nums)
+# print(nums1)
+# print(id(nums1))
+# print(id(nums))
+
+
+# Bitwise AND (&)
+# Bitwise OR (|)
+# Bitwise XOR (^)
+# Bitwise NOT (~)
+# Bitwise left shift (<<)
+# Bitwise right shift (>>)
+
+# A B & | ^ ~
+# 0 0 0 0 0 1
+# 0 1 0 1 1 1 
+# 1 0 0 1 1 0
+# 1 1 1 1 0 0
+
+# print(3 & 5)
+# print(3 | 5)
+# print(3 ^ 5)
+# print(~ 3)
+
+
+# x = 7
+# x = x << 1
+# x = x << 3
+
+print(x)
