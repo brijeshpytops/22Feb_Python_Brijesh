feef2ff6cec755a9c650c32e72fe2c92b124e110
brijeshpytops <brijeshgondaliya.tops@gmail.com>
2024-03-07 08:51:45 +0530

    conditional statements, loops

---
+++ loops/0-for.py
@@ -0,0 +1,21 @@
+# code = "python"
+
+# for ch in code[4:]:
+#     print(ch.upper())
+
+
+# code = "python"
+
+# var = iter(code)
+# print(next(var))
+# print(next(var))
+# print(next(var))
+# print(next(var))
+# print(next(var))
+# print(next(var))
+# print(next(var)) # StopIteration
+
+# range(start, stop+1, step+1)
+
+# print(list(range(1,11, 4)))
+# print(list(range(10,0, -2)))
---
+++ loops/1-while.py
@@ -0,0 +1,27 @@
+# import os
+# import time
+# while(True):
+#     age = input("Enter your age: ")
+#     age = int(age)
+
+
+#     if age >= 18:
+#         weight = input("Enter your weight: ")
+#         weight = float(weight)
+#         if  weight >= 50:
+#             print("You can donate\n\n")
+
+#         else:
+#             print("You can not donate(weight<50)\n\n")
+#     else:
+#         print("You can not donate(age<18)\n\n")
+#     time.sleep(3)
+#     os.system('cls')
+
+# start = 1
+# end = 10
+# table = 300000000006565465455
+
+# while(start<=end):
+#     print(f"{table} * {start} = {start * table}")
+#     start += 1
