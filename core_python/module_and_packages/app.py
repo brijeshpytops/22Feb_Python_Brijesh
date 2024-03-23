6f07c5d55d111d60cc3c27abfe639c96ee092546
brijeshpytops <brijeshgondaliya.tops@gmail.com>
2024-03-23 08:54:34 +0530

    module and package

---
+++ core_python/module_and_packages/app.py
@@ -0,0 +1,35 @@
+import math
+
+# print(dir(math))
+# print(math.factorial(5))
+# print(math.tan(90))
+
+import random
+# print(dir(random))
+# print(random.random())
+# print(random.randrange(1,1000))
+
+# musics = [1,2,3,4,5]
+# random.shuffle(musics)
+# print(musics)
+
+# print(random.randint(111111,999999))
+
+# wn = random.randrange(1,1000)
+# gn = 
+
+# import datetime
+from datetime import datetime, timedelta
+
+current_time = datetime.now()
+
+print(current_time.strftime('%d-%m-%Y, %I:%M:%S %p'))
+# date_ = current_time.date()
+# time_ = current_time.time()
+# print(date_)
+# print(time_)
+
+# past_time = timedelta(days=1, hours=1, minutes=30)
+
+# final_time = current_time + past_time
+# print(final_time)
